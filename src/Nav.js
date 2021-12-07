74b76eee8fa791f630d070f81b8f785349666945
王瑞瑜 <roywang@h2o-team.com>
2021-12-08 03:53:35 +0800

    add runningText

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,13 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const runningText = keyframes`
+  from {
+    transform: translate(100%);
+  }
+  to {
+    transform: translate(-100%);
+  }
+`
 
 const NavWrapper = styled.div`
   display: flex;
@@ -42,14 +51,35 @@ const NavWrapper = styled.div`
     flex-grow: 1;
     height: 50px;
     line-height: 50px;
+    font-size: 18px;
+    white-space: nowrap;
+    overflow: hidden;
+    span {
+      animation: ${runningText} 15s linear infinite;
+    }
+    span::before {
+      content: '';
+      position: absolute;
+      width: 20px;
+      height: 20px;
+      top: 50%;
+      transform: translate(-150%, -50%);
+      background: var(--main);
+    } 
   }
   .list {
+    white-space: nowrap;
     width: 150px;
     height: 50px;
+    font-size: 18px;
+    font-weight: bold;
   }
   .hamberger {
     width: 50px;
     height: 50px;
+    font-size: 30px;
+    background: var(--main);
+    color: white;
   }
 `
 
@@ -59,8 +89,10 @@ export const Nav = () => {
       <div className='logo'>Logo</div>
       <div className='time'>time</div>
       <div className='temp'>temp</div>
-      <div className='banner'>Banner Banner Banner Banner </div>
-      <div className='list'>list</div>
+      <div className='banner'>
+        <span>Every element is a plate, present oneself but also extrude others to create a new possibility.</span>
+      </div>
+      <div className='list'>作 品 列 表</div>
       <div className='hamberger'>=</div>
     </NavWrapper>
   )
